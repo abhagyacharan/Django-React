63bb5d0902ef4e393dea3a26d7f324253f139c85
Bhagya Charan Aela <a.bhagyacharan@gmail.com>
2024-11-16 02:56:59 +0530

    frontend implementation

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer, NoteSerializer
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Note
 
 
@@ -13,12 +13,13 @@ class NoteListCreate(generics.ListCreateAPIView):
     def get_queryset(self):
         user = self.request.user
         return Note.objects.filter(author=user)
-    
+
     def perform_create(self, serializer):
         if serializer.is_valid():
             serializer.save(author=self.request.user)
         else:
             print(serializer.errors)
+
 
 class NoteDelete(generics.DestroyAPIView):
     serializer_class = NoteSerializer
